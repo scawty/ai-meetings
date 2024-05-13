fd7a391ece2ccdfe2cd800d9918f9909084533ff
scawty <cilentow@gmail.com>
2024-05-13 17:15:58 -0500

    update model to GPT4o :)

--- ai-meeting.py
+++ ai-meeting.py
@@ -62,7 +62,7 @@ def get_transcript(
 def extract_references(transcription):
     print("Extracting references...")
     response = client.chat.completions.create(
-        model="gpt-4-turbo",
+        model="gpt-4o",
         temperature=0,
         messages=[
             {
